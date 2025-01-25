022c69c9cdf91a3b82ae97d2aa03b32318fd8d72
siddartha355 <siddarthavemula355@gmail.com>
2025-01-25 10:01:45 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,14 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; 
+        int num2 = 10; 
+
+        System.out.println("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swapping: num1 = " + num1 + ", num2 = " + num2);
+    }
+}
